2ae8810b19a7ab3e3cdc03277a4fb15b8e80c06f
Chitransh Saxena <56127292+Chitransh-Saxena@users.noreply.github.com>
2023-03-04 11:08:12 +0530

    Adding action items and a basic planner

---
+++ Planner.java
@@ -0,0 +1,31 @@
+public class Planner {
+
+    /*
+        March
+            -> Finish DP from leetcode track
+            -> Revise graph and finish Disjoint Set from leetcode.
+
+        April
+            -> Start topics from interviewbit
+                -> No harm in taking time to understand the questions, make diagrams and mappings to absorb the information
+
+        May
+            -> By mid of May, expecting to finish full DSA
+            -> Spend 2 weeks for just LLD and HLD
+                -> Diagrams, class diagrams, class creation
+                -> Understanding which pattern to use under which situation
+
+
+
+
+        Total skill requirement
+            -> DSA
+            -> Java Fundamentals
+                -> Amigoscode how to master java
+            -> LLD
+            -> HLD
+            -> Spring Roadmap
+                -> Have understanding in good breadth, rather than depth.
+
+     */
+}
---
+++ Tough_Tricky_Items.java
@@ -0,0 +1,60 @@
+public class Tough_Tricky_Items {
+
+    /*
+
+        -> Arrays
+            -> In place operations
+                -> Move zeroes
+                -> Sort by parity
+                -> Try more such questions
+            -> Interviewbit Array section
+                -> Have to do it with patience and proper understanding
+            -> Advanced sorting problems
+                -> Custom Comparator
+                -> Comparable and Comparator class
+                    -> Comparable overrides the internal compareTo method and provides a default sorting mechanism
+                        -> Collections.sort(list); will work when some compareTo is given
+
+                    -> Comparator on the other hand, is done in a separate class
+                        -> We create a separate class for some comparison, it implements the Comparator with the object as type we want to compare
+
+                        public class RatingCompare implements Comparator<Movie> {
+
+                            public int compare(Movie m1, Movie m2) {
+
+                                return -1, 0, 1 on the basis of some condition
+                            }
+                        }
+
+                        -> Understand the complexities if any
+
+        -> Trees
+            -> When standard DFS questions are asked to be done with iteration
+                -> inorder, preorder etc.
+            -> When standard BFS questions are asked to be done in DFS manner
+                -> Understand if it can be done
+                -> Where time complexity is better and worse
+                -> Under which situation this might be needed
+
+        -> Heaps
+            -> Take 2-3 days to revise the basics, internals and the use cases
+            -> Practise problems should not take much time.
+
+        -> Design Problems
+            -> Unexplored territory, have not solved many problems here
+
+        -> Bit Manipulation
+            -> Understand the basics once again
+            -> How implementation is done in Java
+
+        -> Backtracking
+            -> Permutation, Combinations and all these
+            -> Understanding and a mapping needs to be created, what kind of changes in code are needed under which condition
+
+        -> Greedy Algorithms
+            -> This topic has been ignored a lot, only did from GFG in the beginning of 2021
+            -> Interviewbit should be sufficient here
+
+
+    */
+}
